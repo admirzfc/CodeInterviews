ce6e6f73d39375f8f549b82ad5b9a3823ab675ee
shanzhihong <zfc114380@163.com>
2019-06-18 21:31:01 +0800

    Q15,the question about if having path of matrix

--- 剑指Offer-Java/Q12-HasPath.java
+++ 剑指Offer-Java/Q12-HasPath.java
@@ -11,7 +11,8 @@ public class HasPath {
     private int[][] next = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-        if (rows == 0 || cols == 0) return false;
+        if (rows == 0 || cols == 0 || matrix.length == 0 || str.length == 0)
+            return false;
         char[][] m = new char[rows][cols];
         for (int i = 0, idx = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -21,20 +22,26 @@ public class HasPath {
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (backtrack(m, rows, cols, str, new boolean[rows][cols], 0, i, j)) return true;
+                if (backtrack(m, rows, cols, str, new boolean[rows][cols], 0, i, j))
+                    return true;
             }
         }
         return false;
     }
 
     private boolean backtrack(char[][] m, int rows, int cols, char[] str, boolean[][] used, int path, int r, int c) {
-        if (path == str.length) return true;
-        if (r < 0 || r >= rows || c < 0 || c >= cols) return false;
-        if (m[r][c] != str[path]) return false;
-        if (used[r][c]) return false;
+        if (path == str.length)
+            return true;
+        if (r < 0 || r >= rows || c < 0 || c >= cols)
+            return false;
+        if (m[r][c] != str[path])
+            return false;
+        if (used[r][c])
+            return false;
         used[r][c] = true;
         for (int i = 0; i < next.length; i++) {
-            if (backtrack(m, rows, cols, str, used, path + 1, r + next[i][0], c + next[i][1])) return true;
+            if (backtrack(m, rows, cols, str, used, path + 1, r + next[i][0], c + next[i][1]))
+                return true;
         }
         used[r][c] = false;
         return false;
